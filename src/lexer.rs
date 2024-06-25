3425f9654cd860505c0da27be5ede9ad867f728b
Armaan Gupta <ohayouarmaan@gmail.com>
2024-06-26 00:47:07 +0530

    looping over source_code

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,13 +1,40 @@
 pub struct Lexer {
     source_code: String,
-    token_types: Vec<tokens::Token>
+    token_types: Vec<tokens::Token>,
+    index: usize,
+    current_line: usize
 }
 
 impl Lexer {
     pub fn new(source_code: String) -> Self {
         Lexer {
             source_code,
-            token_types: vec![]
+            token_types: vec![],
+            index: 0,
+            current_line: 0
+        }
+    }
+
+    fn should_advance(&self) -> bool {
+        if self.index < self.source_code.len() {
+            return true;
+        }
+        return false;
+    }
+
+    fn get_current_character(&self) -> char {
+        if let Some(ch) = self.source_code.chars().nth(self.index) {
+            return ch;
+        } else {
+            return '\0';
+        }
+    }
+
+    pub fn lex(&mut self) {
+        while self.should_advance() {
+            let current_character = self.get_current_character();
+            dbg!(current_character);
+            self.index += 1;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ fn main() {
     let first_argument = arguments.get(1);
     if let Some(file_name) = first_argument {
         let source_code = fs::read_to_string(file_name).expect("[Error]: Can not open specified file.");
-        let _primary_lexer = lexer::Lexer::new(source_code);
+        let mut primary_lexer = lexer::Lexer::new(source_code);
+        primary_lexer.lex();
     } else {
         print_usage();
     }
